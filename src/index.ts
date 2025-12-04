9aa0faab15ddcd666d51bce3aa46dabea2a14a90
mel-bryer <melantheosmb@gmail.com>
2025-12-04 15:19:16 +0700

    Initial setup: Express + TypeScript project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+// Simple route
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Hello from Express + TypeScript! ");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
